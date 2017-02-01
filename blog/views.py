bca8ae893992cb69e558b23ded8eb14ad640df13
revanth <rev4nth@gmail.com>
2017-02-01 12:01:54 +0530

    added classbasedviews

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,15 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 
 from .models import Post
 
-def post_list(request):
-    posts = Post.published.all()
-    paginator = Paginator(posts, 3)
-    page = request.GET.get('page')
-    try:
-        posts =  paginator.page(page)
-    except PageNotAnInteger:
-        posts = paginator.page(1)
-    except EmptyPage:
-        posts = paginator.page(paginator.num_pages)
-        
-    template_path = 'blog/post/list.html'
-    context = {
-        'posts' : posts,
-    }
-    return render(request, template_path, context)
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
 
 
 def post_detail(request, year, month, day, post):
